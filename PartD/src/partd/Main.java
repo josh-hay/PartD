0f3df2fde65a587e42dd6409963b4e758f73205f
Josh Hay <me@joshhay.com.au>
2021-01-27 14:40:18 +1000

    Added my favourite mode of transportation.

--- PartD/src/partd/Main.java
+++ PartD/src/partd/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Josh.");
 		System.out.println("My favourite colour is black.");
+		System.out.println("My favourite mode of transportation is train.");
 	}
 	
 }
