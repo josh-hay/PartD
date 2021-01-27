baeceee8cda17ca9b2a159434b6d8853422bbbe6
Josh Hay <me@joshhay.com.au>
2021-01-27 14:40:48 +1000

    Added the best day of the week.

--- PartD/src/partd/Main.java
+++ PartD/src/partd/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("Hi, my name is Josh.");
 		System.out.println("My favourite colour is black.");
 		System.out.println("My favourite mode of transportation is train.");
+		System.out.println("The best day of the week is thursday.");
 	}
 	
 }
